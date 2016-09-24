6486bdeddf17172c507e94d53e595e20ac57fff3
Ngoguey42 <ngoguey@student.42.fr>
2016-09-24 10:15:50 +0200

    Add localstorage/ directory

---
+++ dart/localstorage/web/main.dart
@@ -0,0 +1,23 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   mixins.dart                                        :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2016/09/17 10:26:42 by ngoguey           #+#    #+#             //
+//   Updated: 2016/09/24 10:10:55 by ngoguey          ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+import 'dart:js' as Js;
+import 'dart:async' as Async;
+import 'dart:html' as Html;
+import 'dart:indexed_db' as Idb;
+import 'dart:math';
+import 'dart:typed_data';
+
+main() {
+  print('Hello World');
+
+}
