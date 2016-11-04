16e3cd4f1b8f074760bfa4a1b96b96b9236c9685
Ngoguey42 <ngoguey@student.42.fr>
2016-11-04 12:30:47 +0100

    Improve debugger style

--- web/debugger/clock_info.dart
+++ web/debugger/clock_info.dart
@@ -6,11 +6,12 @@
 //   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2016/08/25 15:03:15 by ngoguey           #+#    #+#             //
-//   Updated: 2016/10/29 14:59:54 by ngoguey          ###   ########.fr       //
+//   Updated: 2016/11/04 12:27:26 by ngoguey          ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
 import 'dart:html' as Html;
+import 'dart:math' as Math;
 import 'package:ft/ft.dart' as Ft;
 import 'package:emulator/constants.dart';
 import 'package:emulator/emulator.dart' as Emulator;
@@ -21,6 +22,7 @@ import 'package:emulator/emulator.dart' as Emulator;
 
 final Html.HtmlElement _time = Html.querySelector('#debClockTime');
 final Html.HtmlElement _clock = Html.querySelector('#debClock');
+final Html.HtmlElement _lines = Html.querySelector('#debClockLines');
 final Html.HtmlElement _frames = Html.querySelector('#debClockFrames');
 
 /*
@@ -29,16 +31,17 @@ final Html.HtmlElement _frames = Html.querySelector('#debClockFrames');
 
 void _onClockInfo(int clock) {
   final double tMs = clock.toDouble() / GB_CPU_FREQ_DOUBLE * MICROSECONDS_PER_SECOND_DOUBLE;
+  final double l = clock.toDouble() / GB_CLOCK_PER_LINE_DOUBLE;
   final double f = clock.toDouble() / GB_CLOCK_PER_FRAME_DOUBLE;
 
-  // Ft.log('clock_info.dart', '_onClockInfo', [clock]);
-  _clock.text = _clockToString(clock);
   _time.text = new Duration(microseconds: tMs.round()).toString();
-  _frames.text = f.toStringAsPrecision(6);
+  _clock.text = _intToString(clock);
+  _lines.text = _doubleToString(l, 1);
+  _frames.text = _doubleToString(f, 3);
   return ;
 }
 
-String _clockToString(int i) {
+String _intToString(int i) {
   String s = "";
 
   while (true) {
@@ -51,15 +54,21 @@ String _clockToString(int i) {
   }
 }
 
+String _doubleToString(double d, int prec) {
+  String s = _intToString(d ~/ 1);
+  int precVal = Math.pow(10, prec);
+
+  s += '.';
+  s += ((d * precVal).round() % precVal).toString().padLeft(prec, '0');
+  return s;
+}
+
 /*
  * Exposed Methods
  */
 
 void init(Emulator.Emulator emu) {
   Ft.log('clock_info.dart', 'init', [emu]);
-  _time.toString();
-  _clock.toString();
-  _frames.toString();
   assert(_time != null, 'Could not find label time');
   assert(_clock != null, 'Could not find label clock');
   assert(_frames != null, 'Could not find label frames');
